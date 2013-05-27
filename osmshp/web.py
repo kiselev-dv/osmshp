769f60970ad479b1ff6e2a6ca7c07f5e8a5d727b
Aleksandr Dezhin <me@dezhin.net>
2013-05-28 02:22:32 +0400

    Simple region map

--- osmshp/web.py
+++ osmshp/web.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-from osmshp.models import Region, DumpVersion
+from osmshp.models import DBSession, Region, DumpVersion
 
 app = Flask('osmshp')
 
@@ -18,9 +18,21 @@ def index():
 @app.route('/region/<code>')
 def region(code):
     region = Region.filter_by(code=code).one()
+    qgeom = DBSession.execute(
+        """SELECT
+            ST_AsGeoJSON(ST_Difference(geom_out, geom_in )) AS ring,
+            ST_AsGeoJSON(geom) AS geom,
+            ST_XMin(geom_out) AS min_x,
+            ST_XMax(geom_out) AS max_x,
+            ST_YMin(geom_out) AS min_y,
+            ST_YMax(geom_out) AS max_y
+        FROM region WHERE id = :id""",
+        dict(id=region.id)
+    ).fetchone()
     return render_template(
         'region.html',
         region=region,
+        qgeom=qgeom,
         version=DumpVersion.query().one(),
         export_url=app.env.config['web'].get('export_url', None),
     )
